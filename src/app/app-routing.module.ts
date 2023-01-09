16aebef6022ef9459a3437d4bd3fbf4ec246e528
AgustinGodinez <105577729+AgustinGodinez@users.noreply.github.com>
2023-01-08 19:43:16 -0600

    Github pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactosComponent } from './components/contactos/contactos.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  {path:'inicio', component:InicioComponent, data:{breadcrumb:'inicio'}},
+  {path:'contacto', component:ContactosComponent, data:{breadcrumb: 'contacto'} },
+  {path:'nosotros',component:NosotrosComponent, data:{breadcrumb: 'nosotros'}},
+  {path:'**', redirectTo:'/inicio', pathMatch:'full'}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash:true} )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ContactosComponent } from './components/contactos/contactos.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { HeaderComponent } from './components/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NosotrosComponent,
+    ContactosComponent,
+    InicioComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
